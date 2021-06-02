cb63e91987ed3153d4ac83a80e597c6042d9acfa
Ekenayy <ekenayy@gmail.com>
2021-06-02 00:13:11 -0400

    added paypal buttons and calllback functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import React from 'react'
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
 
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
 function App() {
+
+  function createOrder(data, actions) {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "1",
+          },
+        },
+      ],
+    });
+  }
+
+  async function onApprove(data, actions) {
+    let order = await actions.order.capture();
+    console.log(order);
+    return order;
+  }
+
+  function onError(err) {
+    console.log(err)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+        onError={(err) => onError(err)}
+        onCancel={() => onError('Canceled')}
+      />
     </div>
   );
 }
