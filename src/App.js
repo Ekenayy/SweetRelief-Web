a1a671e425fcd8994d188b647031728c8d06dbd3
Ekenayy <ekenayy@gmail.com>
2021-06-07 13:10:40 -0400

    added link to ReactNative app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import './App.css';
 
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
+// When the page opens up it should automatically be fetching from a rails route
+// This route will contain all the info that will ultimatey go into the create order function
+
 function App() {
 
+  const windowUrl = window.location.search
+  const params = new URLSearchParams(windowUrl)
+
+  // Send information to this create order function
   function createOrder(data, actions) {
     return actions.order.create({
       purchase_units: [
@@ -22,11 +29,19 @@ function App() {
   async function onApprove(data, actions) {
     let order = await actions.order.capture();
     console.log(order);
+    window.ReactNativeWebView &&
+      window.ReactNativeWebView.postMessage(JSON.stringify(order))
     return order;
   }
 
   function onError(err) {
     console.log(err)
+    let errObj = {
+      err: err,
+      status: "FAILED",
+    };
+    window.ReactNativeWebView &&
+      window.ReactNativeWebView.postMessage(JSON.stringify(errObj));
   }
 
   return (
@@ -37,6 +52,7 @@ function App() {
         onError={(err) => onError(err)}
         onCancel={() => onError('Canceled')}
       />
+      <p>{params}</p>
     </div>
   );
 }
