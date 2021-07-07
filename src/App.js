94b52e3b84b7006c226232465e16046b08e9070e
Ekenayy <ekenayy@gmail.com>
2021-07-07 17:02:44 -0400

    adjusted firebase.json

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ import Intro from './components/Intro'
 import Product from './components/Product'
 import About from './components/About'
 
-
-
-const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
-
 function App() {
 
   return (
