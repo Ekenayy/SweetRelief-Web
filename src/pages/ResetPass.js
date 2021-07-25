f1e66ae07088efbec806a9f8dd4a9788f19e7e2b
Ekenayy <ekenayy@gmail.com>
2021-07-25 19:35:40 -0400

    styled forgot password form

--- src/pages/ResetPass.js
+++ src/pages/ResetPass.js
@@ -3,15 +3,124 @@ import styled from 'styled-components';
 
 function ResetPass () {
 
+    const [error, setError] = useState("")
+
     const Body = styled.div`
+        width: 100%;
+        height: 100vh;
+        background: #BEA7E5;
+        background: -webkit-radial-gradient(top left, #BEA7E5, #F4A261);
+        background: -moz-radial-gradient(top left, #BEA7E5, #F4A261);
+        background: radial-gradient(to bottom right, #BEA7E5, #F4A261);
     `
 
-    const Text = styled.p`
+    const ResetPage = styled.div`
+        width: 460px;
+        padding: 8% 0 0;
+        margin: auto;
+    `
+
+    const Form = styled.div`
+        position: relative;
+        z-index: 1;
+        background: #FFFFFF;
+        max-width: 460px;
+        margin: 0 auto 100px;
+        padding: 45px;
+        border-radius: 10px;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    `
+
+    const Input = styled.input`
+        outline: 0;
+        font-family: 'aileronregular', 'Roboto', sans-serif;
+        background: #e2dee2;
+        width: 100%;
+        border: 0;
+        margin: 0 0 15px;
+        padding: 15px;
+        box-sizing: border-box;
+        font-size: 14px;
+        border-radius: 10px;
+    `
+
+    const InputSection = styled.div`
+
+    `
+
+    const TitleText = styled.p`
+        text-align: center;
+        font-size: 20px;
+        font-family: 'aileronregular', 'Roboto', sans-serif;
+    `
+
+    const InputText = styled.p`
+        left: 0;
+        font-family: 'aileronregular', 'Roboto', sans-serif;
+    `
+
+    const Button = styled.button`
+        font-family: 'aileronregular', 'Roboto', sans-serif;
+        text-transform: uppercase;
+        outline: 0;
+        background: #BEA7E5;
+        width: 100%;
+        border: 0;
+        padding: 15px;
+        color: #FFFFFF;
+        font-size: 14px;
+        -webkit-transition: all 0.3 ease;
+        transition: all 0.3 ease;
+        cursor: pointer;
+        margin-top: 10px;
+        border-radius: 10px;
+        &:hover, &:active, &:focus {
+            background: #9b89ba;
+        }
+    `
+
+    const ErorrSpan = styled.p`
+        color: red;
+        font-family: 'aileronregular', 'Roboto', sans-serif;
     `
 
     return (
         <Body>
-            <Text>Hello from reset pass</Text>
+            <ResetPage>
+                <Form>
+                    <TitleText>Reset Password</TitleText>
+                    <InputSection>
+                        <InputText>Token:</InputText>
+                        <Input
+                            type='text'
+                            placeholder='token'
+                        />
+                    </InputSection>
+                    <InputSection>
+                        <InputText>Email:</InputText>
+                        <Input
+                            type='text'
+                            placeholder='email'
+                        />
+                    </InputSection>
+                    <InputSection>
+                        <InputText>New Password:</InputText>
+                        <Input
+                            type='password'
+                            placeholder='new password'
+                        />
+                    </InputSection>
+                    <InputSection>
+                        <InputText>Confirm New Password:</InputText>
+                        <Input
+                            type='password'
+                            placeholder='confirm password'
+                        />
+                    </InputSection>
+                    {error ? <ErorrSpan>Test</ErorrSpan> : null}
+                    <Button>Submit</Button>
+                </Form>
+            </ResetPage>
         </Body>
     )
 }
