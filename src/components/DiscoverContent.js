0d7b3310b2d671b7c322aa0c1ba3a26215f086f8
Ekenayy <ekenayy@gmail.com>
2021-08-04 17:13:27 -0500

    created conditional for discoverContent component based off of screen size

--- src/components/DiscoverContent.js
+++ src/components/DiscoverContent.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import {AllContentDiv, BigText, PicDiv, DemoImage} from '../css/styles/Styles.js'
 import LocationDetails from '../photos/LocationDetails.png'
 import DiscoverScreen from '../photos/DiscoverScreen.png'
+import { useMediaQuery } from 'react-responsive';
 
 const DiscoverDiv = styled.div`
         display: flex;
@@ -11,13 +12,14 @@ const DiscoverDiv = styled.div`
     `
 
 const DiscoverPicDiv = styled(PicDiv)`
-    @media (max-width: 37.5em) {
-        max-height: 450px;
-        max-width: 250px;
-        position: absolute;
-        left: 30px;
-        bottom: 30px;
-    }
+`
+
+const MobilePicDiv = styled(PicDiv)`
+    max-height: 450px;
+    max-width: 250px;
+    position: absolute;
+    left: 30px;
+    bottom: 30px;
 `
 
 const OverlayPicDiv = styled(DiscoverPicDiv)`
@@ -30,34 +32,74 @@ const OverlayPicDiv = styled(DiscoverPicDiv)`
     }
 `
 
+const MobileOverlay = styled(MobilePicDiv)`
+    position: relative;
+    left: 100px;
+    top: 30px;
+    bottom: 0px;
+    opacity: 0.95;
+`
+
 const DiscoverMedia = styled.div`
         display: flex;
         flex-direction: row;
         padding-left: 50px;
         height: 100%;
+        align-items: center;
+        align-self: center;
         @media screen and (max-width: 37.5em) {
             flex-direction: column;
             position: relative;
             padding-right: 80px;
         }
-    `
+`
+
+const DiscoverMobile = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    padding-right: 80px;
+    height: 100%;
+    padding-left: 50px;
+`
 
 const DiscoverText = styled(BigText)`
 `
 
 function DiscoverContent () {
 
-    return (
-        <DiscoverDiv>
-                <DiscoverText>Discover hidden gems and promotions in your area</DiscoverText>
+    const isMobile = useMediaQuery({ query: `(max-width: 37.5em)` });
+
+    const MobileMedia = ( ) => {
+        return (
+            <DiscoverMobile>
+                <MobilePicDiv>
+                    <DemoImage src={LocationDetails}></DemoImage>
+                </MobilePicDiv>
+                <MobileOverlay>
+                    <DemoImage src={DiscoverScreen}></DemoImage>
+                </MobileOverlay>
+            </DiscoverMobile>
+        )
+    }
+
+    const WebMedia = () => {
+        return (
             <DiscoverMedia>
                 <DiscoverPicDiv>
                     <DemoImage src={LocationDetails}></DemoImage>
                 </DiscoverPicDiv>
-                <OverlayPicDiv>
+                <DiscoverPicDiv>
                     <DemoImage src={DiscoverScreen}></DemoImage>
-                </OverlayPicDiv>
+                </DiscoverPicDiv>
             </DiscoverMedia>
+        )
+    }
+
+    return (
+        <DiscoverDiv>
+                <DiscoverText>Discover hidden gems and promotions in your area</DiscoverText>
+                {isMobile ? <MobileMedia/> : <WebMedia/>}
         </DiscoverDiv>
     )
 
