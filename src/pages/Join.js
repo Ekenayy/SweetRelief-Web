7628f8861e728ddc98afeb46de8f4ec65b583b12
Ekenayy <ekenayy@gmail.com>
2021-08-16 16:03:14 -0400

    created content div

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -4,7 +4,9 @@ import {AllContentDiv, BigText, PicDiv, DemoImage} from '../css/styles/Styles.js
 import Header from '../components/Header.js'
 import { useHistory } from "react-router-dom"
 
-const ContentDiv = styled.div`
+const Body = styled.div`
+        display: flex;
+        flex-direction: column;
         margin-top: 5rem;
         width: 100%;
         height: 100%;
@@ -15,21 +17,35 @@ const ContentDiv = styled.div`
         background: -moz-radial-gradient(top left, #BEA7E5, #F4A261);
         background: radial-gradient(to bottom right, #BEA7E5, #F4A261);
         @media screen and (max-width: 37.5em) {
-            display: flex;
             padding: 0px;
-            height: 100%;
         }
 `
 
-const Body = styled.div`
-`
-
 const MainDiv = styled.div`
-        padding-top: 4rem;
+    padding-top: 4rem;
+`
+
+const ContentDiv = styled.div`
+    align-self: center;
+    margin-top: 80px;
+    width: 90%;
+    display: flex;
+    border-radius: 40px;
+    padding-top: 4rem;
+    flex-direction: row;
+    position: relative;
+    z-index: 1;
+    background: #FFFFFF;
+    @media screen and (max-width: 37.5em) {
+        flex-direction: column;
+    }
+`
+
+const WordDiv = styled.div`
 `
 
 
-function Join ( {location} ) {
+function Join ( ) {
 
     // How this will work section
     // 1. Fill out this quick form
@@ -43,13 +59,11 @@ function Join ( {location} ) {
     return (
         <Body>
             <ContentDiv>
-                <MainDiv>
-                    <BigText>Welcome to Join</BigText>
-                </MainDiv>
+                <WordDiv>
+                    <BigText>Welcome</BigText>
+                </WordDiv>
             </ContentDiv>
-            
-        </Body>
-        
+        </Body>        
     )
 }
 
